e1a058dd36d27238a9d1e164c0a3534060a965fe
Nathan Santos <natan-silva-2011@hotmail.com>
2022-12-08 14:55:42 -0300

    Criando janela de componentes

--- Componentes/Form1.Designer.cs
+++ Componentes/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Componentes
 {
-    partial class Form1
+    partial class frmFrutas
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,245 @@ namespace Componentes
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFrutas));
+            this.cbbFrutasListadas = new System.Windows.Forms.ComboBox();
+            this.ltbFrutas = new System.Windows.Forms.ListBox();
+            this.gpbFrutas = new System.Windows.Forms.GroupBox();
+            this.lblBanana = new System.Windows.Forms.Label();
+            this.lblMaca = new System.Windows.Forms.Label();
+            this.lblPera = new System.Windows.Forms.Label();
+            this.lblMelancia = new System.Windows.Forms.Label();
+            this.lblSair = new System.Windows.Forms.Label();
+            this.txtFrutaSelecionada = new System.Windows.Forms.TextBox();
+            this.lblFrutaSelecionada = new System.Windows.Forms.Label();
+            this.lblFrutaCombo = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtSelecionar = new System.Windows.Forms.TextBox();
+            this.lblFrutasListadas = new System.Windows.Forms.Label();
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gpbFrutas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbbFrutasListadas
+            // 
+            this.cbbFrutasListadas.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbFrutasListadas.FormattingEnabled = true;
+            this.cbbFrutasListadas.Location = new System.Drawing.Point(344, 338);
+            this.cbbFrutasListadas.Name = "cbbFrutasListadas";
+            this.cbbFrutasListadas.Size = new System.Drawing.Size(223, 28);
+            this.cbbFrutasListadas.TabIndex = 6;
+            // 
+            // ltbFrutas
+            // 
+            this.ltbFrutas.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltbFrutas.FormattingEnabled = true;
+            this.ltbFrutas.ItemHeight = 20;
+            this.ltbFrutas.Location = new System.Drawing.Point(344, 123);
+            this.ltbFrutas.Name = "ltbFrutas";
+            this.ltbFrutas.Size = new System.Drawing.Size(223, 144);
+            this.ltbFrutas.TabIndex = 7;
+            // 
+            // gpbFrutas
+            // 
+            this.gpbFrutas.Controls.Add(this.lblSair);
+            this.gpbFrutas.Controls.Add(this.lblMaca);
+            this.gpbFrutas.Controls.Add(this.lblBanana);
+            this.gpbFrutas.Controls.Add(this.lblMelancia);
+            this.gpbFrutas.Controls.Add(this.lblPera);
+            this.gpbFrutas.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbFrutas.Location = new System.Drawing.Point(21, 103);
+            this.gpbFrutas.Name = "gpbFrutas";
+            this.gpbFrutas.Size = new System.Drawing.Size(200, 189);
+            this.gpbFrutas.TabIndex = 8;
+            this.gpbFrutas.TabStop = false;
+            this.gpbFrutas.Text = "Frutas";
+            // 
+            // lblBanana
+            // 
+            this.lblBanana.AutoSize = true;
+            this.lblBanana.Location = new System.Drawing.Point(6, 21);
+            this.lblBanana.Name = "lblBanana";
+            this.lblBanana.Size = new System.Drawing.Size(63, 20);
+            this.lblBanana.TabIndex = 0;
+            this.lblBanana.Text = "1-Banana";
+            // 
+            // lblMaca
+            // 
+            this.lblMaca.AutoSize = true;
+            this.lblMaca.Location = new System.Drawing.Point(6, 41);
+            this.lblMaca.Name = "lblMaca";
+            this.lblMaca.Size = new System.Drawing.Size(52, 20);
+            this.lblMaca.TabIndex = 0;
+            this.lblMaca.Text = "2-Maçã";
+            // 
+            // lblPera
+            // 
+            this.lblPera.AutoSize = true;
+            this.lblPera.Location = new System.Drawing.Point(6, 61);
+            this.lblPera.Name = "lblPera";
+            this.lblPera.Size = new System.Drawing.Size(46, 20);
+            this.lblPera.TabIndex = 0;
+            this.lblPera.Text = "3-Pêra";
+            // 
+            // lblMelancia
+            // 
+            this.lblMelancia.AutoSize = true;
+            this.lblMelancia.Location = new System.Drawing.Point(6, 81);
+            this.lblMelancia.Name = "lblMelancia";
+            this.lblMelancia.Size = new System.Drawing.Size(72, 20);
+            this.lblMelancia.TabIndex = 0;
+            this.lblMelancia.Text = "4-Melância";
+            // 
+            // lblSair
+            // 
+            this.lblSair.AutoSize = true;
+            this.lblSair.Location = new System.Drawing.Point(6, 101);
+            this.lblSair.Name = "lblSair";
+            this.lblSair.Size = new System.Drawing.Size(42, 20);
+            this.lblSair.TabIndex = 0;
+            this.lblSair.Text = "5-Sair";
+            // 
+            // txtFrutaSelecionada
+            // 
+            this.txtFrutaSelecionada.Location = new System.Drawing.Point(21, 343);
+            this.txtFrutaSelecionada.Name = "txtFrutaSelecionada";
+            this.txtFrutaSelecionada.Size = new System.Drawing.Size(200, 20);
+            this.txtFrutaSelecionada.TabIndex = 1;
+            // 
+            // lblFrutaSelecionada
+            // 
+            this.lblFrutaSelecionada.AutoSize = true;
+            this.lblFrutaSelecionada.Location = new System.Drawing.Point(21, 327);
+            this.lblFrutaSelecionada.Name = "lblFrutaSelecionada";
+            this.lblFrutaSelecionada.Size = new System.Drawing.Size(96, 13);
+            this.lblFrutaSelecionada.TabIndex = 0;
+            this.lblFrutaSelecionada.Text = "Fruta Selecionada:";
+            // 
+            // lblFrutaCombo
+            // 
+            this.lblFrutaCombo.AutoSize = true;
+            this.lblFrutaCombo.Location = new System.Drawing.Point(341, 322);
+            this.lblFrutaCombo.Name = "lblFrutaCombo";
+            this.lblFrutaCombo.Size = new System.Drawing.Size(104, 13);
+            this.lblFrutaCombo.TabIndex = 0;
+            this.lblFrutaCombo.Text = "Selecione uma fruta:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(124, 22);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(307, 18);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Digite o número para selecionar a fruta:";
+            // 
+            // txtSelecionar
+            // 
+            this.txtSelecionar.Location = new System.Drawing.Point(128, 45);
+            this.txtSelecionar.Name = "txtSelecionar";
+            this.txtSelecionar.Size = new System.Drawing.Size(303, 20);
+            this.txtSelecionar.TabIndex = 0;
+            // 
+            // lblFrutasListadas
+            // 
+            this.lblFrutasListadas.AutoSize = true;
+            this.lblFrutasListadas.Location = new System.Drawing.Point(341, 107);
+            this.lblFrutasListadas.Name = "lblFrutasListadas";
+            this.lblFrutasListadas.Size = new System.Drawing.Size(74, 13);
+            this.lblFrutasListadas.TabIndex = 0;
+            this.lblFrutasListadas.Text = "Frutas listadas";
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.Location = new System.Drawing.Point(61, 384);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(89, 43);
+            this.btnSelecionar.TabIndex = 2;
+            this.btnSelecionar.Text = "&Selecionar";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(326, 384);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(89, 43);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(190, 384);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(89, 43);
+            this.btnInserir.TabIndex = 3;
+            this.btnInserir.Text = "&Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "&Voltar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmFrutas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(593, 439);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSelecionar);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblFrutasListadas);
+            this.Controls.Add(this.lblFrutaCombo);
+            this.Controls.Add(this.lblFrutaSelecionada);
+            this.Controls.Add(this.txtSelecionar);
+            this.Controls.Add(this.txtFrutaSelecionada);
+            this.Controls.Add(this.gpbFrutas);
+            this.Controls.Add(this.ltbFrutas);
+            this.Controls.Add(this.cbbFrutasListadas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmFrutas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frutas";
+            this.gpbFrutas.ResumeLayout(false);
+            this.gpbFrutas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbbFrutasListadas;
+        private System.Windows.Forms.ListBox ltbFrutas;
+        private System.Windows.Forms.GroupBox gpbFrutas;
+        private System.Windows.Forms.Label lblSair;
+        private System.Windows.Forms.Label lblMaca;
+        private System.Windows.Forms.Label lblBanana;
+        private System.Windows.Forms.Label lblMelancia;
+        private System.Windows.Forms.Label lblPera;
+        private System.Windows.Forms.TextBox txtFrutaSelecionada;
+        private System.Windows.Forms.Label lblFrutaSelecionada;
+        private System.Windows.Forms.Label lblFrutaCombo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtSelecionar;
+        private System.Windows.Forms.Label lblFrutasListadas;
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button button1;
     }
 }
 
